43c550ce8ff46ebff8ab7e99b484710cb6083643
OsoMania <suspensethebc@gmail.com>
2020-11-30 16:38:28 -0600

    styling done

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     ## my apps
     'learning_logs',
     'users',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
